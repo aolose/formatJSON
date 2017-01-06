d90ce2c8fa8c1b51c9591557ae66f0e95b075674
Aolose <aolose@qq.com>
2017-01-06 14:24:45 +0800

    Update formatJSON.js

--- formatJSON.js
+++ formatJSON.js
@@ -6,26 +6,27 @@
  * @param {number} space 空格个数 代表一个tab 
  * @return {string} 返回格式化的字符串
  **/
-function formatJSON(o,space=2) {
+function formatJSON(o,space = 2) {
   let f = function (n,o) {
     let str = '';
-    const tab =' '.repeat(space);
+    const tab = ' '.repeat(space);
     const tabs = tab.repeat(n);
     let type = typeof o;
     if (type === 'object' && Array.isArray(o)) type = 'array';
     switch (type) {
       case 'array':
-        return str += '[' + o.map(m => f(n + 1,m)).join() + ']'
+        return str += '[' + o.map(m => f(n + 1,m)).join() + ']';
       case 'number':
         return o;
       case 'string':
-        return '"' + o + '"';
+        return JSON.stringify(o);
       case 'object':
         let ks = Object.keys(o);
         return str += '{\n' + tabs + ks.map(m => '  "' + m + '":'
           + f(n + 1,o[m])).join(',\n' + tabs)
           + '\n' + tabs + '}'
     }
-  }
+  };
   return f(0,o);
 }
+
